0e1727cbc7a22cad15aa6911d41c58665a7b5df5
Aman Kurmangaliev <94127505+yetyoursons@users.noreply.github.com>
2022-08-06 14:18:35 +0600

    Add files via upload

---
+++ hw_6/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import StatusUserPage from './pages/statusUserPage/StatusUserPage';
+
+
+function App() {
+  return (
+    <div className="container">
+      <StatusUserPage/>
+    </div>
+
+  );
+}
+export default App;
